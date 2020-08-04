026bc3463c02810ab8c109115deac66f92a19a40
Nathan Steed <steedn@outlook.com>
2020-08-04 22:44:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
 class Dog(name, breed)
-  
+  def dname
+    @name
+  end
+  def dname = name
+    dname = @name
+  end
+
+  def dbreed
+    @breed
+  end
+  def dbreed = breed
+    dbreed = @breed
+  end
+end
